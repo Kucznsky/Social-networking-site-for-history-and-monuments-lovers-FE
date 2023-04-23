d0b2a17d6926476948826918c3638f6e14141ee2
Kucznsky <michal.kuczynski2000@wp.pl>
2023-04-23 16:17:44 +0200

    added a type to the localisation mock

--- monument-ui/src/app/mocks/localisation.mock.ts
+++ monument-ui/src/app/mocks/localisation.mock.ts
@@ -1,4 +1,6 @@
-export const LocalisationMock = {
+import { LocalisationResponse } from "../interfaces/localisation.interface";
+
+export const LocalisationMock: LocalisationResponse = {
   latitude: '53.109859479223644',
   longtitude: '23.204819610298653',
   locationName: 'Białystok, Podlaskie, Poland',
