4181a7c633f8ed8661e39c53537c3448ca03ad88
Kucznsky <michal.kuczynski2000@wp.pl>
2023-12-17 12:51:20 +0100

    added functions to the service

--- monument-ui/src/app/services/post-api.service.ts
+++ monument-ui/src/app/services/post-api.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { PostResponse } from '../interfaces/post.interface';
+import { POST_URL } from '../consts/endpoint-url.const';
+import { UsersPost } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +11,24 @@ import { Observable, of } from 'rxjs';
 export class PostApiService {
   constructor(private http: HttpClient) {}
 
-  public fetchAllPosts(): void {
-    
+  public fetchAllPosts(): Observable<PostResponse[]> {
+    return this.http.get<PostResponse[]>(`${POST_URL}/all`)
   }
 
-  public fetchPost(id: string): void {
-    //work in progress - backend is not ready yet
+  public fetchPost(id: string): Observable<PostResponse> {
+    return this.http.get<PostResponse>(`${POST_URL}/${id}`)
   }
 
-  public deletePost(id: string): void {
-    //work in progress - backend is not ready yet
+  public deletePost(id: string): Observable <void> {
+    return this.http.delete<void>(`${POST_URL}/${id}`)
   }
 
-  public uploadPost(): void {
-    //work in progress - backend is not ready yet
+  public uploadPost(body: UsersPost): Observable<PostResponse> {
+    return this.http.post<PostResponse>(`${POST_URL}`, body)
   }
 
-  public editPost(id: string): void {
+  public editPost(body: UsersPost, id: string): Observable<PostResponse> {
+    return this.http.put<PostResponse>(`${POST_URL}/${id}`, body)
     //I'm not sure yet, if I'll allow to edit posts in the first version of the app
   }
 }
