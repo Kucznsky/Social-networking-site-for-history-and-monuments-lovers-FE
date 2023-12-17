7d8f57dbf4e6d8698ef238f7aecfdbeaf0564d2d
Kucznsky <michal.kuczynski2000@wp.pl>
2023-12-17 12:34:40 +0100

    added enums representing monument category

---
+++ monument-ui/src/app/enums/post-category.enum.ts
@@ -0,0 +1,7 @@
+export enum Category {
+  MilitaryStructures = 'Military structures',
+  ReligiousObjects = 'Religious objects',
+  PalacesAndVillas = 'Palaces and villas',
+  IndustrialObjects = 'Industrial objects ',
+  Other = 'Other',
+}
