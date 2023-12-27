ba4cc35c39f7de4f6ff4dd3d95df4308e74de62d
Kucznsky <michal.kuczynski2000@wp.pl>
2023-12-28 00:27:21 +0100

    styled login and register modals

--- monument-ui/src/app/components/login/login.component.ts
+++ monument-ui/src/app/components/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  public loginFormGroup: UntypedFormGroup = new UntypedFormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
+  public logIn(): void {}
 }
--- monument-ui/src/app/components/navigation-bar/navigation-bar.component.ts
+++ monument-ui/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss'],
 })
-export class NavigationBarComponent {
-  constructor(private readonly router: Router) {}
+export class NavigationBarComponent implements OnInit {
+  public isUserLoggedIn = false;
+
+  constructor(
+    private readonly router: Router,
+    private readonly localStorageService: LocalStorageService,
+  ) {}
+
+  public ngOnInit(): void {}
+
   public redirectToHomePage(): void {
     this.router.navigateByUrl('/home');
   }
