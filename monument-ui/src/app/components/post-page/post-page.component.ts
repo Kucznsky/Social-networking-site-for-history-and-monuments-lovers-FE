396c270aa534daac449c5ea5eb34ee014288ee6e
Kucznsky <michal.kuczynski2000@wp.pl>
2024-01-07 15:21:49 +0100

    post-page component - work in progress

--- monument-ui/src/app/components/post-page/post-page.component.ts
+++ monument-ui/src/app/components/post-page/post-page.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { UsersPost } from 'src/app/models';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostPageComponent {}
+export class PostPageComponent implements OnInit, OnDestroy{
+  public postId: string
+  public postData: UsersPost
+  private readonly unsubscriber: Subject<void> = new Subject();
+
+  constructor(private readonly postService: PostService,     private readonly activatedRoute: ActivatedRoute,     private readonly changeDetectorRef: ChangeDetectorRef,
+
+    ){}
+
+  public ngOnInit(): void {
+    this.postId = this.activatedRoute.snapshot.paramMap.get('id')
+    this.postService.getPostById(this.postId).pipe(takeUntil(this.unsubscriber)).subscribe((post)=>{this.postData = post; this.changeDetectorRef.markForCheck()})
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
+}
--- monument-ui/src/app/services/post.service.ts
+++ monument-ui/src/app/services/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map, take } from 'rxjs';
 import { UsersPost } from '../models';
 import { PostApiService } from './post-api.service';
 
@@ -7,19 +7,30 @@ import { PostApiService } from './post-api.service';
   providedIn: 'root',
 })
 export class PostService {
-  private posts: BehaviorSubject<UsersPost[]> = new BehaviorSubject<
+  private listOfPosts: BehaviorSubject<UsersPost[]> = new BehaviorSubject<
     UsersPost[]
   >([]);
+  private post: BehaviorSubject<UsersPost> = new BehaviorSubject<
+  UsersPost
+>(undefined);
 
   constructor(private postApiService: PostApiService) {}
 
   public getAllPosts(): void {
     this.postApiService.fetchAllPosts().subscribe((posts) => {
-      this.posts.next(posts.map((post) => new UsersPost(post)));
+      this.listOfPosts.next(posts.map((post) => new UsersPost(post)));
     });
   }
 
   public getPostsObservable(): Observable<UsersPost[]> {
-    return this.posts.asObservable();
+    return this.listOfPosts.asObservable();
   }
+
+  public getPostById(postId: string): Observable<UsersPost> {
+    return this.postApiService.fetchPost(postId).pipe(map((post)=>new UsersPost(post)))
+  }
+
+  // public getPostValue(): UsersPost {
+  //   return this.post.getValue()
+  // }
 }
