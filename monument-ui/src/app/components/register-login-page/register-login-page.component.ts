88d7a039be2dbf591919063f94f5cf0ead31a711
Kucznsky <michal.kuczynski2000@wp.pl>
2024-01-14 02:01:48 +0100

    added logic to the auth component

--- monument-ui/src/app/components/register-login-page/register-login-page.component.ts
+++ monument-ui/src/app/components/register-login-page/register-login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { JwtService } from '../../services/jwt.service';
+import { UserAuthService } from '../../services/user-auth.service';
 
 @Component({
   selector: 'app-register-login-page',
@@ -8,9 +11,16 @@ import { Router } from '@angular/router';
 })
 export class RegisterLoginPageComponent implements OnInit{
   public isLoginFormVisible: boolean;
+  public youDontHaveAnAccountMessage = `You don't have an account?`
+  public youAlreadyHaveAnAccountMessage = `You already have an account?`
 
+  public authFormGroup: FormGroup = new FormGroup({
+    userName: new FormControl<string>('', [Validators.required]),
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [Validators.required]),
+  });
 
-  constructor(private router : Router){}
+  constructor(private router : Router, private readonly userAuthService: UserAuthService){}
 
   public ngOnInit(): void {
     this.isStringOrRegisterFormOpen();
@@ -20,5 +30,32 @@ export class RegisterLoginPageComponent implements OnInit{
     const urlFragment = new RegExp('login')
 
     this.isLoginFormVisible = urlFragment.test(this.router.url)
+  }
+
+  public loginOrRegister(): void {
+    if(this.isLoginFormVisible){
+      this.logIn()
+    } else {
+      this.register()
+    }
+  }
+
+  private logIn(): void {
+    if (this.authFormGroup.get('email').valid && this.authFormGroup.get('password').valid) {
+      this.userAuthService.login(
+        this.authFormGroup.controls['email'].getRawValue(),
+        this.authFormGroup.controls['password'].getRawValue(),
+      );
+    }
+  }
+
+  public register(): void {
+    if (this.authFormGroup.valid) {
+      this.userAuthService.register(
+        this.authFormGroup.controls['userName'].getRawValue(),
+        this.authFormGroup.controls['email'].getRawValue(),
+        this.authFormGroup.controls['password'].getRawValue(),
+      );
+    }
   }
 }
