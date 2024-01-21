c8aa728ae230e00682ab452ced7805fe39c867a0
Kucznsky <michal.kuczynski2000@wp.pl>
2024-01-21 18:40:55 +0100

    added error alert

--- monument-ui/src/app/components/register-login-page/register-login-page.component.ts
+++ monument-ui/src/app/components/register-login-page/register-login-page.component.ts
@@ -1,32 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { JwtService } from '../../services/jwt.service';
 import { UserAuthService } from '../../services/user-auth.service';
+import { Subject, filter, of, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-register-login-page',
   templateUrl: './register-login-page.component.html',
   styleUrls: ['./register-login-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterLoginPageComponent implements OnInit {
+export class RegisterLoginPageComponent implements OnInit, OnDestroy {
   public isLoginFormVisible: boolean;
+  public errorMessage = '';
   public youDontHaveAnAccountMessage = `You don't have an account?`;
   public youAlreadyHaveAnAccountMessage = `You already have an account?`;
 
   public authFormGroup: FormGroup = new FormGroup({
     userName: new FormControl<string>('', [Validators.required]),
     email: new FormControl<string>('', [Validators.required, Validators.email]),
-    password: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
+
+  private readonly unsubscriber: Subject<void> = new Subject();
 
   constructor(
     private router: Router,
     private readonly userAuthService: UserAuthService,
+    private readonly changeDetectorRef: ChangeDetectorRef,
   ) {}
 
   public ngOnInit(): void {
     this.isStringOrRegisterFormOpen();
+    this.observeErrorMessageObs();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
   }
 
   private isStringOrRegisterFormOpen(): void {
@@ -67,9 +87,24 @@ export class RegisterLoginPageComponent implements OnInit {
 
   public isFormErroneous(fieldName: string): boolean {
     return (
-      (this.authFormGroup.get(fieldName).errors) &&
+      this.authFormGroup.get(fieldName).errors &&
       (this.authFormGroup.get(fieldName).dirty ||
         this.authFormGroup.get(fieldName).touched)
     );
+  }
+
+  private observeErrorMessageObs(): void {
+    this.userAuthService
+      .getErrorMessageObservable()
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((message) => {
+        this.errorMessage = message;
+        if (this.errorMessage !== '') {
+          setTimeout(() => {
+            this.userAuthService.showErrorMessage('');
+          }, 2000);
+        }
+        this.changeDetectorRef.markForCheck();
+      });
   }
 }
--- monument-ui/src/app/services/user-auth.service.ts
+++ monument-ui/src/app/services/user-auth.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
 import { AuthApiService } from './auth-api.service';
 import { LocalStorageKeys } from '../enums';
-import { BehaviorSubject, Observable, finalize, take } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  finalize,
+  take,
+  throwError,
+} from 'rxjs';
 import { User } from '../models';
 import { UserApiService } from './user-api.service';
 import { JwtService } from './jwt.service';
@@ -15,6 +22,9 @@ export class UserAuthService {
   private loggedUser: BehaviorSubject<any> = new BehaviorSubject<any>(
     undefined,
   );
+  private errorMessage: BehaviorSubject<string> = new BehaviorSubject<string>(
+    '',
+  );
 
   constructor(
     private readonly authApiService: AuthApiService,
@@ -24,20 +34,37 @@ export class UserAuthService {
   ) {}
 
   public register(userName: string, email: string, password: string): void {
+    let failed: boolean;
     this.authApiService
       .register({ email: email, password: password, userName: userName })
-      .pipe(take(1))
+      .pipe(
+        take(1),
+        catchError((e) => {
+          failed = true;
+          this.showErrorMessage(e.error.message);
+          return [];
+        }),
+      )
       .subscribe(() => {
         this.router.navigate(['user-registered']);
       });
   }
 
   public login(email: string, password: string): void {
+    let failed: boolean;
     this.authApiService
       .login({ email: email, password: password })
       .pipe(
+        take(1),
+        catchError((e) => {
+          failed = true;
+          this.showErrorMessage(e.error.message);
+          return [];
+        }),
         finalize(() => {
-          window.location.reload();
+          if (!failed) {
+            window.location.reload();
+          }
         }),
       )
       .subscribe((response) => {
@@ -66,5 +93,13 @@ export class UserAuthService {
 
   public resetLoggedUserData(): void {
     this.loggedUser.next(null);
+  }
+
+  public showErrorMessage(mesage: string): void {
+    this.errorMessage.next(mesage);
+  }
+
+  public getErrorMessageObservable(): Observable<string> {
+    return this.errorMessage.asObservable();
   }
 }
