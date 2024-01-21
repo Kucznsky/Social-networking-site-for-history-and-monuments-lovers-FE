b26b9ae4efb01a2916ca2e1d40a7c3e01965a8fc
Kucznsky <michal.kuczynski2000@wp.pl>
2024-01-21 20:01:05 +0100

    modified comment item

--- monument-ui/src/app/components/comment-list-item/comment-list-item.component.ts
+++ monument-ui/src/app/components/comment-list-item/comment-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { UserComment } from 'src/app/models';
 
 @Component({
@@ -6,6 +7,30 @@ import { UserComment } from 'src/app/models';
   templateUrl: './comment-list-item.component.html',
   styleUrls: ['./comment-list-item.component.scss'],
 })
-export class CommentListItemComponent {
+export class CommentListItemComponent implements OnInit {
   @Input() commentData: UserComment;
+
+  public userAvatar;
+  public usersPlaceholerInitials: string
+
+  constructor(
+    private domSanitizer: DomSanitizer,
+  ) {}
+
+  public ngOnInit(): void {
+    this.sanitizeImageUrl();
+  }
+
+  private sanitizeImageUrl(): void {
+    if(this.commentData.author.avatar){
+      this.userAvatar = this.domSanitizer.bypassSecurityTrustUrl(
+        this.commentData.author.avatar,
+      );
+    } else {
+      this.userAvatar = null;
+      this.usersPlaceholerInitials = this.commentData.author.userName
+        .charAt(0)
+        .toUpperCase();
+    }
+  }
 }
