ed8ad9eeca82e5581a1638a33ef7949f6da3ef42
Kucznsky <michal.kuczynski2000@wp.pl>
2024-01-28 16:13:32 +0100

    modified new post interfave

--- monument-ui/src/app/models/newPost.model.ts
+++ monument-ui/src/app/models/newPost.model.ts
@@ -1,21 +1,12 @@
 import { Category } from '../enums';
 
 export class NewPost {
-  // public id: string = '';
-  // public title: string = '';
-  // public category: Category = Category.Other;
-  // public likes: number = 0;
-  // public isLiked: boolean = false;
-  // public usersCommentsId: string[] = [];
-  // public author: User = new User;
-  // public localisation: Localisation = new Localisation();
-  // public thumbnail: string = '';
-  // public modernImages: string[] = [];
-  // public archivalImages: string[] = [];
-  // public description: string = '';
   category: Category = Category.Other;
   title = '';
   content = '';
   localisation = '';
   authorId = '';
+  thumbnail: string;
+  oldPictures? :string []
+  modernPictures?:string[]
 }
